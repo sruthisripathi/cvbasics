c034f9b6bc227bfd0bd3eb63ad1c0153eeb587fc
sruthi sripathi <sruthisripathi7@gmail.com>
2022-11-11 09:13:25 +0530

    color spaces and thresholding

---
+++ color_spaces/color_diff.py
@@ -0,0 +1,5 @@
+import math
+red_green = math.sqrt(((255 - 0) ** 2) + ((0 - 255) ** 2) + ((0 - 0) ** 2))
+red_purple = math.sqrt(((255 - 128) ** 2) + ((0 - 0) ** 2) + ((0 - 128) ** 2))
+red_navy = math.sqrt(((255 - 0) ** 2) + ((0 - 0) ** 2) + ((0 - 128) ** 2))
+print(red_green, red_purple, red_navy)
